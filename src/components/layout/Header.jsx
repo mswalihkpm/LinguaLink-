93b3b240faf99cb5e4e0bbde460db78a4a2fc79a
MUHAMMAD SWALIH MT <mswalihkpm@gmail.com>
2025-06-30 13:34:51 +0530

    Update Header.jsx

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => (
         <img 
           src="https://i.postimg.cc/2jbmzYGy/Chat-GPT-Image-Jun-30-2025-01-25-55-PM-removebg-preview.png" 
           alt="LinguaLink Logo" 
-          className="w-10 h-10 rounded-2xl mx-auto"
+          className="w-15 h-15 rounded-2xl mx-auto"
         />
       </motion.div>
       
